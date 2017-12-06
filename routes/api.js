f7fddbd99a602137d6cd2b1c77259e8f3f2eb60e
dinukanadeeshan <dinuka.nadeeshan1993@gmail.com>
2017-12-06 17:08:10 +0530

    testing the content-type with application/json

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 
 router.get('/v1', (req, res, next) => {
-  res.send('hello there');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ data: 'hello there' }));
 });
 
+
+router.post('/v2', (req, res, next) => {
+  console.log(req.body.name);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ data: 'this is good response' }));
+});
 module.exports = router;
